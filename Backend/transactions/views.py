e55b837617dc5908fb67b17862bde27f12875392
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-11-27 22:23:56 +0800

    changes in transactions views

--- Backend/transactions/views.py
+++ Backend/transactions/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from .serializers import TransactionSerializer
+from .models import Transaction
+from config.settings import DEBUG
 
-# Create your views here.
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    if (not DEBUG):
+        permission_classes = [IsAuthenticated]
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
