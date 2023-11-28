f16686992672624c6ce22de14d1a3b3b75825671
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-11-28 10:44:02 +0800

    added components in api urls

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
+    path('accounts/', include('accounts.urls')),
+    path('equipments/', include('equipments.urls')),
+    path('transactions/', include('transactions.urls'))
 ]
