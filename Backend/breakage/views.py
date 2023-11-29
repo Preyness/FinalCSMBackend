c0aa659c507764592ad65d8b69b2f11ffab88e31
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-11-29 22:47:59 +0800

    changes in breakage views

--- Backend/breakage/views.py
+++ Backend/breakage/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import BreakageReport
+from .serializers import BreakageReportSerializer
+from rest_framework.permissions import IsAuthenticated
+from config.settings import DEBUG
 
 # Create your views here.
+
+
+class BreakageReportViewSet(viewsets.ModelViewSet):
+    if (not DEBUG):
+        permission_classes = [IsAuthenticated]
+    serializer_class = BreakageReportSerializer
+    queryset = BreakageReport.objects.all()
