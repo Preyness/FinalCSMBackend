ea52ec3dd2673ccb8a7c93cb7314ee4598430064
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-11-29 22:45:48 +0800

    changes in breakage models

--- Backend/breakage/models.py
+++ Backend/breakage/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from accounts.models import CustomUser
+from transactions.models import Transaction
+from equipments.models import EquipmentInstance
+from django.utils.timezone import now
 # Create your models here.
+#
+
+
+class BreakageReport(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('REIMBURSED', 'Reimbursed'),
+        ('REPLACED', 'Replaced'),
+    )
+
+    transaction = models.ForeignKey(
+        Transaction, on_delete=models.CASCADE)
+    equipments = models.ManyToManyField(EquipmentInstance)
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    timestamp = models.DateTimeField(default=now, editable=False)
+
+    def __str__(self):
+        return f"Breakage report for transaction {self.borrower}"
