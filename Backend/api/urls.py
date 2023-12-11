69e2a11280d974fbd8f6d7b51e873f66667df3f6
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:11:20 +0800

    added a new path in API

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('equipments/', include('equipments.urls')),
     path('transactions/', include('transactions.urls')),
-    path('breakages/', include('breakages.urls')),
+    path('breakage/', include('breakage.urls')),
 ]
