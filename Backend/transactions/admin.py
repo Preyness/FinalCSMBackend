42db38f28a29e9098f0e5181d37d0e7456c0d8f0
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:55:38 +0800

    changes in transactions admin

--- Backend/transactions/admin.py
+++ Backend/transactions/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Transaction
+from accounts.models import CustomUser
 
-# Register your models here.
-admin.site.register(Transaction)
+
+class TransactionAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "borrower":
+            kwargs["queryset"] = CustomUser.objects.exclude(
+                is_technician=True).exclude(is_teacher=True)
+        elif db_field.name == "teacher":
+            kwargs["queryset"] = CustomUser.objects.filter(is_teacher=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Transaction, TransactionAdmin)
