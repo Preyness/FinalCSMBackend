1ed879dace32949e57d30d8cc0dbd16e9c48d392
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:23:41 +0800

    changes in equipments models and serializers

--- Backend/equipments/models.py
+++ Backend/equipments/models.py
@@ -3,12 +3,17 @@ from django.utils.timezone import now
 from simple_history.models import HistoricalRecords
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-# Create your models here.
 
 
 class Equipment(models.Model):
+    EQUIPMENT_CATEGORY_CHOICES = (
+        ('Glassware', 'Glassware'),
+        ('Miscellaneous', 'Miscellaneous')
+    )
     name = models.CharField(max_length=40)
-    description = models.TextField(max_length=512)
+    category = models.CharField(
+        max_length=20, choices=EQUIPMENT_CATEGORY_CHOICES, default='Miscellaneous')
+    description = models.TextField(max_length=512, null=True)
     date_added = models.DateTimeField(default=now, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
     history = HistoricalRecords()
@@ -18,22 +23,15 @@ class Equipment(models.Model):
 
 
 class EquipmentInstance(models.Model):
-    STATUS_CHOICES = (
+    EQUIPMENT_INSTANCE_STATUS_CHOICES = (
         ('Working', 'Working'),
         ('Broken', 'Broken'),
         ('Borrowed', 'Borrowed'),
     )
-    ROOM_CHOICES = (
-        ('ROOM1', 'ROOM1'),
-        ('ROOM2', 'ROOM2'),
-        ('ROOM3', 'ROOM3'),
-    )
-
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='PENDING')
+        max_length=20, choices=EQUIPMENT_INSTANCE_STATUS_CHOICES, default='PENDING')
     remarks = models.TextField(max_length=512, null=True)
-    room = models.CharField(max_length=20, choices=ROOM_CHOICES)
     date_added = models.DateTimeField(default=now, editable=False)
     last_updated = models.DateTimeField(auto_now=True, editable=False)
     history = HistoricalRecords()
@@ -46,10 +44,14 @@ class EquipmentInstance(models.Model):
 def create_superuser(sender, **kwargs):
     if sender.name == 'equipments':
         EQUIPMENT, CREATED = Equipment.objects.get_or_create(
-            name="Beaker", description="Generic Beaker")
-        if (CREATED):
-            print("Created sample equipment")
+            name="Pyrex Beaker", description="", category="Glassware")
         EQUIPMENT_INSTANCE, CREATED = EquipmentInstance.objects.get_or_create(
-            equipment=EQUIPMENT, status="Working", remarks="First beaker of USTP!")
-        if (CREATED):
-            print("Created sample equipment instance")
+            equipment=EQUIPMENT, status="Working", remarks="First beaker of equipment tracker!")
+        EQUIPMENT, CREATED = Equipment.objects.get_or_create(
+            name="Bunsen Burner", description="", category="Miscellaneous")
+        EQUIPMENT_INSTANCE, CREATED = EquipmentInstance.objects.get_or_create(
+            equipment=EQUIPMENT, status="Working", remarks="First bunsen burner of equipment tracker!")
+        EQUIPMENT, CREATED = Equipment.objects.get_or_create(
+            name="Microscope", description="", category="Miscellaneous")
+        EQUIPMENT_INSTANCE, CREATED = EquipmentInstance.objects.get_or_create(
+            equipment=EQUIPMENT, status="Working", remarks="First microscope of equipment tracker!")
--- Backend/equipments/serializers.py
+++ Backend/equipments/serializers.py
@@ -1,7 +1,9 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
+from rest_framework import serializers, exceptions
 from .models import Equipment, EquipmentInstance
-
+from drf_spectacular.utils import extend_schema_field
+from drf_spectacular.types import OpenApiTypes
+from django.db.models import F
+from breakage.models import BreakageReport
 # -- Equipment Serializers
 
 
@@ -9,7 +11,7 @@ class EquipmentHistoricalRecordField(serializers.ListField):
     child = serializers.DictField()
 
     def to_representation(self, data):
-        return super().to_representation(data.values('name', 'description', 'history_date', 'history_user').order_by('-history_date'))
+        return super().to_representation(data.values('name', 'description', 'category', 'history_date', 'history_user').order_by('-history_date'))
 
 
 class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
@@ -23,16 +25,42 @@ class EquipmentSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Equipment
-        fields = ('id', 'name', 'description',
+        fields = ('id', 'name', 'description', 'category',
                   'last_updated', 'last_updated_by', 'date_added')
         read_only_fields = ('id', 'last_updated',
                             'last_updated_by', 'date_added')
 
+    def create(self, instance, validated_data):
+        user = self.context['request'].user
+        # Do not allow users that are not technicians to create equipments
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot create equipments")
+        return super().create(instance, validated_data)
+
+    def update(self, instance, validated_data):
+        user = self.context['request'].user
+        # Do not allow users that are not technicians to update equipments
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot update equipments")
+        return super().update(instance, validated_data)
+
+    # Do not allow users that are not technicians to delete equipments
+    def delete(self, instance):
+        user = self.context['request'].user
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot delete equipments")
+        instance.delete()
+
+    @extend_schema_field(OpenApiTypes.STR)
     def get_history_user(self, obj):
         return obj.history_user.username if obj.history_user else None
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_last_updated_by(self, obj):
-        return obj.history.first().history_user if obj.history.first().history_user else None
+        return obj.history.first().history_user.username if obj.history.first().history_user else None
 
 
 class EquipmentLogsSerializer(serializers.HyperlinkedModelSerializer):
@@ -42,11 +70,12 @@ class EquipmentLogsSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Equipment.history.model
-        fields = ('history_id', 'name', 'description',
+        fields = ('history_id', 'id', 'name', 'description', 'category',
                   'history_date', 'history_user')
         read_only_fields = ('history_id', 'id', 'name', 'description',
                             'history_date', 'history_user')
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_history_user(self, obj):
         return obj.history_user.username if obj.history_user else None
 
@@ -58,18 +87,19 @@ class EquipmentLogSerializer(serializers.HyperlinkedModelSerializer):
         format="%m-%d-%Y %I:%M%p", read_only=True)
     last_updated_by = serializers.SerializerMethodField()
     name = serializers.CharField()
-    description = serializers.CharField()
+    description = serializers.CharField(required=False)
     history = EquipmentHistoricalRecordField()
 
     class Meta:
         model = Equipment
-        fields = ('id', 'name', 'description',
+        fields = ('id', 'name', 'description', 'category',
                   'last_updated', 'date_added', 'last_updated_by', 'history')
         read_only_fields = ('id', 'last_updated',
                             'date_added', 'last_updated_by', 'history')
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_last_updated_by(self, obj):
-        return obj.history.first().history_user if obj.history.first().history_user else None
+        return obj.history.first().history_user.username if obj.history.first().history_user else None
 
 # -- Equipment Instance Serializers
 
@@ -78,62 +108,122 @@ class EquipmentInstanceHistoricalRecordField(serializers.ListField):
     child = serializers.DictField()
 
     def to_representation(self, data):
-        return super().to_representation(data.values('equipment', 'status', 'remarks', 'history_date', 'history_user').order_by('-history_date'))
+        data = data.annotate(equipment_name=F('equipment__name'))
+        data = data.annotate(category=F('equipment__category'))
+        return super().to_representation(data.values('equipment', 'equipment_name', 'category', 'status', 'remarks', 'history_date', 'history_user').order_by('-history_date'))
 
 
 class EquipmentInstanceSerializer(serializers.HyperlinkedModelSerializer):
-    equipment = serializers.PrimaryKeyRelatedField(
-        source='equipment.name', queryset=Equipment.objects.all())
+    equipment = serializers.SlugRelatedField(
+        slug_field='id', queryset=Equipment.objects.all())
+    equipment_name = serializers.CharField(
+        source='equipment.name', read_only=True)
+    category = serializers.CharField(
+        source='equipment.category', read_only=True)
     status = serializers.CharField()
-    remarks = serializers.CharField(allow_null=True, default=None)
+    remarks = serializers.CharField(required=False)
     date_added = serializers.DateTimeField(
         format="%m-%d-%Y %I:%M%p", read_only=True)
     last_updated = serializers.DateTimeField(
         format="%m-%d-%Y %I:%M%p", read_only=True)
     last_updated_by = serializers.SerializerMethodField()
-    status = serializers.ChoiceField(choices=EquipmentInstance.STATUS_CHOICES)
+    status = serializers.ChoiceField(
+        choices=EquipmentInstance.EQUIPMENT_INSTANCE_STATUS_CHOICES)
 
-    # Forbid user from changing equipment field once the instance is already created
+    def create(self, instance, validated_data):
+        user = self.context['request'].user
+        # Do not allow users that are not technicians to create equipment instances
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot create equipments")
+        return super().create(instance, validated_data)
+
     def update(self, instance, validated_data):
+        user = self.context['request'].user
+        # Do not allow users that are not technicians to update equipment instances
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot update equipment instances")
+        # Forbid user from changing equipment field once the instance is already created
         # Ignore any changes to 'equipment'
         validated_data.pop('equipment', None)
-        return super().update(instance, validated_data)
+
+        # This is for Breakage Report handling
+        # First we update the EquipmentInstance
+        instance = super().update(instance, validated_data)
+        # Then we check if the EquipmentInstance has an associated BreakageReport which is still pending
+        associated_breakage_report = BreakageReport.objects.filter(
+            equipments=instance, resolved=False).first()
+        # If there is one
+        if associated_breakage_report:
+            # Check if all the equipments of the currently associated BreakageReport are "Working"
+            all_working = all(
+                eq.status == 'Working' for eq in associated_breakage_report.equipments.all())
+
+            # If all the equipments are "Working", set Breakage Report to be resolved (resolved=True)
+            if all_working:
+                associated_breakage_report.resolved = True
+                associated_breakage_report.save()
+
+        return instance
+
+    # Do not allow users that are not technicians to delete equipment instances
+    def delete(self, instance):
+        user = self.context['request'].user
+        if not user.is_technician:
+            raise exceptions.ValidationError(
+                "Non-technician users cannot delete equipment instances")
+        instance.delete()
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['equipment'] = instance.equipment.__str__()
+        return representation
 
     class Meta:
         model = EquipmentInstance
-        fields = ('id', 'equipment', 'status', 'room', 'remarks',
+        fields = ('id', 'equipment', 'equipment_name', 'category', 'status', 'remarks',
                   'last_updated', 'last_updated_by', 'date_added')
-        read_only_fields = ('id', 'last_updated',
-                            'last_updated_by', 'date_added')
+        read_only_fields = ('id', 'last_updated', 'equipment_name', 'category',
+                            'last_updated_by', 'date_added', 'equipment_name')
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_history_user(self, obj):
         return obj.history_user.username if obj.history_user else None
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_last_updated_by(self, obj):
-        return obj.history.first().history_user if obj.history.first().history_user else None
+        return obj.history.first().history_user.username if obj.history.first().history_user else None
 
 
 class EquipmentInstanceLogsSerializer(serializers.HyperlinkedModelSerializer):
     history_date = serializers.DateTimeField(
         format="%m-%d-%Y %I:%M%p", read_only=True)
     history_user = serializers.SerializerMethodField()
-    equipment = serializers.PrimaryKeyRelatedField(
-        source='equipment.name', queryset=Equipment.objects.all())
+    equipment = serializers.SlugRelatedField(
+        slug_field='id', queryset=Equipment.objects.all())
+    equipment_name = serializers.CharField(
+        source='equipment.name', read_only=True)
+    category = serializers.CharField(
+        source='equipment.category', read_only=True)
 
     class Meta:
         model = EquipmentInstance.history.model
-        fields = ('history_id', 'id', 'equipment', 'status',  'room', 'remarks',
+        fields = ('history_id', 'id', 'equipment', 'equipment_name', 'category', 'status', 'remarks',
                   'history_date', 'history_user')
-        read_only_fields = ('history_id', 'id', 'equipment', 'status', 'remarks',
-                            'history_date', 'history_user')
+        read_only_fields = ('history_id', 'id', 'equipment', 'equipment_name', 'category', 'status', 'remarks',
+                            'history_date', 'history_user', 'equipment_name')
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_history_user(self, obj):
         return obj.history_user.username if obj.history_user else None
 
 
 class EquipmentInstanceLogSerializer(serializers.HyperlinkedModelSerializer):
-    equipment = serializers.PrimaryKeyRelatedField(
-        source='equipment.name', queryset=Equipment.objects.all())
+    equipment = serializers.SlugRelatedField(
+        slug_field='id', queryset=Equipment.objects.all())
+    equipment_name = serializers.CharField(
+        source='equipment.name', read_only=True)
     status = serializers.CharField()
     remarks = serializers.CharField()
     date_added = serializers.DateTimeField(
@@ -142,6 +232,8 @@ class EquipmentInstanceLogSerializer(serializers.HyperlinkedModelSerializer):
         format="%m-%d-%Y %I:%M%p", read_only=True)
     last_updated_by = serializers.SerializerMethodField()
     history = EquipmentInstanceHistoricalRecordField()
+    category = serializers.CharField(
+        source='equipment.category', read_only=True)
 
     # Forbid user from changing equipment field once the instance is already created
     def update(self, instance, validated_data):
@@ -151,10 +243,11 @@ class EquipmentInstanceLogSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = EquipmentInstance
-        fields = ('id', 'equipment', 'status', 'room', 'remarks',
+        fields = ('id', 'equipment', 'equipment_name', 'category', 'status', 'remarks',
                   'last_updated', 'date_added', 'last_updated_by', 'history')
-        read_only_fields = ('id', 'last_updated',
-                            'date_added', 'last_updated_by', 'history')
+        read_only_fields = ('id', 'last_updated', 'equipment_name', 'category',
+                            'date_added', 'last_updated_by', 'history', 'equipment_name')
 
+    @extend_schema_field(OpenApiTypes.STR)
     def get_last_updated_by(self, obj):
-        return obj.history.first().history_user if obj.history.first().history_user else None
+        return obj.history.first().history_user.username if obj.history.first().history_user else None
