81c3e4bb30ad8d5b6ff0654caa75e7215f7c9978
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:58:04 +0800

    changes in transactions models and serializers

--- Backend/transactions/models.py
+++ Backend/transactions/models.py
@@ -3,20 +3,37 @@ from accounts.models import CustomUser
 from equipments.models import EquipmentInstance
 from django.utils.timezone import now
 
+
 class Transaction(models.Model):
-    STATUS_CHOICES = (
-        ('APPROVED', 'Approved'),
-        ('RETURNED', 'Returned'),
-        ('REJECTED', 'Rejected'),
-        ('PENDING', 'Pending'),
+    TRANSACTION_STATUS_CHOICES = (
+        # Transaction is pending approval
+        ('Pending Approval', 'Pending Approval'),
+        # Transaction has been approved, pending delivery by labtech
+        ('Approved', 'Approved'),
+        # Tranasction has been rejected
+        ('Rejected', 'Rejected'),
+        # Transaction has been approved but has been cancelled due to rare circumstances
+        ('Cancelled', 'Cancelled'),
+        # Transaction has been delivered and is on borrow
+        ('Borrowed', 'Borrowed'),
+        # Transaction has been returned, pending checking
+        ('Returned: Pending Checking', 'Returned: Pending Checking'),
+        # Transaction has been breakages after being returned, pending resolution
+        ('With Breakage: Pending Resolution',
+            'With Breakage: Pending Resolution'),
+        # Transaction has been finalized
+        ('Finalized', 'Finalized'),
     )
 
     borrower = models.ForeignKey(
-        CustomUser, on_delete=models.SET_NULL, null=True)
+        CustomUser, on_delete=models.SET_NULL, null=True, related_name='borrowed_transactions')
+    remarks = models.TextField(max_length=512, null=True)
+    teacher = models.ForeignKey(
+        CustomUser, on_delete=models.SET_NULL, null=True, related_name='teacher_transactions')
     equipments = models.ManyToManyField(EquipmentInstance)
-    status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    transaction_status = models.CharField(
+        max_length=40, choices=TRANSACTION_STATUS_CHOICES, default='Pending')
     timestamp = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
-        return f"Transaction by {self.borrower}"
+        return f"Transaction #{self.id} under {self.teacher} by {self.borrower}"
--- Backend/transactions/serializers.py
+++ Backend/transactions/serializers.py
@@ -1,74 +1,176 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from accounts.models import CustomUser
 from equipments.models import EquipmentInstance
-from equipments.serializers import EquipmentInstanceSerializer
 from .models import Transaction
+from breakage.models import BreakageReport
 from accounts.models import CustomUser
 from config.settings import DEBUG
 
 
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
     borrower = serializers.SlugRelatedField(
-        many=False, slug_field='id', queryset=CustomUser.objects.all(), required=True, allow_null=True)
+        many=False, slug_field='id', queryset=CustomUser.objects.all(), required=False, allow_null=True)
 
     equipments = serializers.SlugRelatedField(
-        many=True, slug_field='id', queryset=EquipmentInstance.objects.filter(status="Working"))
+        many=True, slug_field='id', queryset=EquipmentInstance.objects.filter(status="Working"), required=False)
+
+    transaction_status = serializers.ChoiceField(
+        choices=Transaction.TRANSACTION_STATUS_CHOICES)
 
     class Meta:
         model = Transaction
-        fields = ['id', 'borrower', 'equipments', 'status', 'timestamp']
+        fields = ['id', 'borrower', 'teacher',
+                  'equipments', 'transaction_status', 'timestamp']
         read_only_fields = ['id', 'timestamp']
 
-    def create(self, validated_data):
-        user = self.context['request'].user
-        if DEBUG and user.is_anonymous:
-            admin = CustomUser.objects.filter(username="admin").first()
-            validated_data['borrower'] = admin
-        else:
-            validated_data['borrower'] = user
+    # Do not allow deletion of transactions
+    def delete(self):
+        raise exceptions.ValidationError(
+            "Deletion of transactions is not allowed. Please opt to cancel a transaction or finalize it")
 
-        validated_data['status'] = 'PENDING'
+    def create(self, validated_data):
+        # Any transactions created will be associated with the one sending the POST/CREATE request
+        user = self.context['request'].user
+        validated_data.data['borrower'] = user
+
+        # All created transactions will be labelled as Pending
+        validated_data['transaction_status'] = 'Pending'
+
+        # If no teacher responsible for the borrow transaction is selected, raise an error
+        if 'teacher' not in validated_data:
+            raise serializers.ValidationError(
+                "You have not selected a teacher responsible for your borrow transaction")
+
+        # If no borrower responsible for the borrow transaction is selected, raise an error
+        if 'borrower' not in validated_data:
+            raise serializers.ValidationError(
+                "No borrower assigned for this transaction!")
+
+        # If the user in the teacher field is not actually a teacher, raise an error
+        borrower = validated_data.get('borrower')
+        if borrower and borrower.is_teacher or borrower.is_technician:
+            raise serializers.ValidationError(
+                "The borrower must be a student. Not a teacher or techician")
+
+        # If the user in the teacher field is not actually a teacher, raise an error
+        teacher = validated_data.get('teacher')
+        if teacher and not teacher.is_teacher:
+            raise serializers.ValidationError(
+                "The assigned teacher is not a valid teacher")
+
+        # If the user in the teacher field is not actually a teacher, raise an error
+        teacher = validated_data.get('teacher')
+        if teacher and not teacher.is_teacher:
+            raise serializers.ValidationError(
+                "The specified user is not a teacher.")
+
+        # If there are no equipments specified, raise an error
         if 'equipments' in validated_data and validated_data['equipments'] == []:
             raise serializers.ValidationError(
                 "You cannot create a transaction without any equipments selected"
             )
 
-        equipments = validated_data.get('equipments', [])
+       # Check if any of the equipment instances are already in a non-finalized transaction
+        equipments = validated_data['equipments']
         for equipment in equipments:
-            equipment.status = 'Borrowed'
-            equipment.save()
+            existing__pending_transactions = Transaction.objects.filter(
+                equipments=equipment, status__in=['Pending', 'Approved', 'Borrowed', 'With Breakage: Pending Resolution'])
+            if existing__pending_transactions.exists():
+                raise serializers.ValidationError(
+                    f"Cannot add Equipment #{equipment.id}. It is still part of a non-finalized transaction")
+
         return super().create(validated_data)
 
     def update(self, instance, validated_data):
         user = self.context['request'].user
-        if not DEBUG:
-            if not user.is_teacher and 'status' in validated_data and validated_data['status'] != instance.status:
-                raise serializers.ValidationError(
-                    "You do not have permission to change the status of a transaction."
-                )
 
+        # If user is not a teacher or a technician, forbid them from changing the status of a transaction
+        if not user.is_teacher and not user.technician and 'transaction_status' in validated_data and validated_data['transaction_status'] != instance.status:
+            raise serializers.ValidationError(
+                "You are not a teacher or technician. You do not have permission to change the status of transactions"
+            )
+
+        # If user is not a teacher, forbid them from changing the status of a transaction
+        if not user.is_teacher and 'transaction_status' in validated_data and validated_data['transaction_status'] != instance.status:
+            raise serializers.ValidationError(
+                "You do not have permission to change the status of a transaction"
+            )
+        # Do not allow changes to equipments on created transactions
         if 'equipments' in validated_data and instance.equipments != validated_data['equipments']:
             raise serializers.ValidationError(
                 "You cannot change the equipments of an already created transaction"
             )
-        if 'status' in validated_data and instance.status == 'PENDING' and validated_data['status'] == 'RETURNED':
+
+        # For already finalized/done transactions (Rejected or Finalized ones)
+        # Do not allow any changes to already finalized transactions
+        if instance.status in ['Rejected', 'Finalized']:
             raise serializers.ValidationError(
-                "You cannot change a PENDING transaction to RETURNED"
+                "Unable to update rejected or finalized transaction. Please create a new one"
             )
 
-        if 'status' in validated_data and instance.status in ['REJECTED', 'RETURNED'] and validated_data['status'] not in ['REJECTED', 'RETURNED']:
-            raise serializers.ValidationError(
-                "You cannot change a rejected transaction anymore"
-            )
+        # Check if the update involves the transaction status
+        if 'transaction_status' in validated_data:
 
-        # Check if the transaction status is being updated to RETURNED or REJECTED
-        if validated_data['status'] in ['RETURNED', 'REJECTED']:
-            # Get the equipment instances related to the transaction
-            equipment_instances = instance.equipments.all()
+            # For Pending transactions
+            # If not changing to Approved or Rejected, throw an error
+            if instance.status == 'Pending' and validated_data['transaction_status'] != 'Approved' or validated_data['transaction_status'] != 'Rejected':
+                raise serializers.ValidationError(
+                    "A pending transaction can only change to Approved or Rejected"
+                )
 
-            # Iterate through the equipment instances and set their status to 'Working'
-            for equipment_instance in equipment_instances:
-                equipment_instance.status = 'Working'
-                equipment_instance.save()
+            # For Approved transactions,
+            # If not changing to Borrowed or Cancelled, throw an error
+            # Already approved transactions can only be moved to Borrowed or Cancelled
+            if instance.status == 'Approved' and validated_data['transaction_status'] != 'Borrowed' or validated_data != 'Cancelled':
+                raise serializers.ValidationError(
+                    "An already approved transaction can only changed to Borrowed (On borrow) or Cancelled"
+                )
+
+            # For Borrowed transactions,
+            # If not changing to returned, throw an error
+            # Borrowed transactions that can only be changed to returned, pending checking for broken items
+            if instance.status == 'Borrowed' and validated_data['transaction_status'] != 'Finalized' or validated_data != 'With Breakage: Pending Resolution':
+                raise serializers.ValidationError(
+                    "A borrowed transaction can only changed to status of Finalized or With Breakage: Pending Resolution"
+                )
+
+            # For Return: Pending Checking transactions,
+            # If not changing to With Breakages: Pending Resolution or Finalized, throw an error
+            # Returned transactions can only be finalized without any issues or be marked as with breakages
+            if instance.status == 'Returned: Pending Checking' and validated_data['transaction_status'] != 'Finalized' or validated_data != 'With Breakage: Pending Resolution':
+                raise serializers.ValidationError(
+                    "A borrowed transaction can only changed to status of Finalized or With Breakage: Pending Resolution"
+                )
+
+            # For transactions with pending breakage resolutions,
+            # Do not allow updating of status. It should be updated within its respective breakage report
+            # If it has been resolved there, this field will automatically update to Finalized
+            if instance.status == 'With Breakage: Pending Resolution':
+                raise serializers.ValidationError(
+                    "A transaction with pending breakage resolutions must be updated or resolved in its respective breakage report"
+                )
+
+            # If there are no issues and a transaction changes from Approved to Borrowed, label the selected equipment's statuses as Borrowed
+            if instance.status == 'Approved' and validated_data['transaction_status'] == 'Borrowed':
+                equipments = validated_data.get('equipments', [])
+                for equipment in equipments:
+                    equipment.status = 'Borrowed'
+                    equipment.save()
+                return super().update(validated_data)
+            # If the transaction changes from Borrowed to Finalized and there are no breakages, label the selected equipment's statuses as Working again from Borrowed
+            if instance.status == 'Borrowed' and validated_data['transaction_status'] == 'Finalized':
+                equipments = validated_data.get('equipments', [])
+                for equipment in equipments:
+                    equipment.status = 'Working'
+                    equipment.save()
+                return super().update(validated_data)
+            # If the transaction changes from Borrowed to With Breakages, we create a Breakage Report instance
+            if instance.status == 'Borrowed' and validated_data['transaction_status'] == 'Finalized':
+                BreakageReport.objects.create(
+                    transaction=instance,
+                    equipments=instance.equipments.all(),
+                    resolved=False
+                )
+            # Changing equipment status of broken items when there are breakages is handled in breakage reports
 
         return super().update(instance, validated_data)
