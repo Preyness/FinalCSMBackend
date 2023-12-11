9a239b214de0942f6d20dba054fff2480b6bb26c
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:01:33 +0800

    changes in admin accounts

--- Backend/accounts/admin.py
+++ Backend/accounts/admin.py
@@ -3,21 +3,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-class CustomUserForm(forms.ModelForm):
-    is_student =  forms.BooleanField(required=False)
-    is_technician = forms.BooleanField(required=False)
-
-    class Meta:
-        model = CustomUser
-        fields = '__all__'
-
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    form = CustomUserForm
-
+    list_display = UserAdmin.list_display + ('is_technician', 'is_teacher')
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('is_teacher','is_technician')}),
+        (None, {'fields': ('is_technician', 'is_teacher')}),
     )
 
 
