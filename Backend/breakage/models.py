c3c9816ca370992f15f8bd358ba292107421872f
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:14:09 +0800

    changes in breakage models

--- Backend/breakage/models.py
+++ Backend/breakage/models.py
@@ -3,23 +3,44 @@ from accounts.models import CustomUser
 from transactions.models import Transaction
 from equipments.models import EquipmentInstance
 from django.utils.timezone import now
-# Create your models here.
-#
 
 
 class BreakageReport(models.Model):
-    STATUS_CHOICES = (
-        ('PENDING', 'Pending'),
-        ('REIMBURSED', 'Reimbursed'),
-        ('REPLACED', 'Replaced'),
-    )
-
     transaction = models.ForeignKey(
         Transaction, on_delete=models.CASCADE)
     equipments = models.ManyToManyField(EquipmentInstance)
-    status = models.CharField(
-        max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    resolved = models.BooleanField(default=False)
     timestamp = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
-        return f"Breakage report for transaction {self.borrower}"
+        return f"Breakage report for transaction #{self.transaction.id} by {self.transaction.borrower} under {self.transaction.teacher}"
+
+    def save(self, *args, **kwargs):
+        # Check if the instance is being updated
+        if not self._state.adding:
+            # Check if all associated equipment instances have status "Working"
+            all_working = all(
+                eq.status == 'Working' for eq in self.equipments.all())
+
+            # If all equipment instances are working
+            if all_working:
+                # set resolved field to True
+                self.resolved = True
+                # set the status of the associated transaction to "Finalized"
+                self.transaction.status = 'Finalized'
+                self.transaction.save()
+
+                # Then save the instance again to reflect the changes
+                super().save(*args, **kwargs)
+            # If not, set the resolved field to False
+            else:
+                if (self.resolved != False or self.transaction.status != 'With Breakages: Pending Resolution'):
+                    # set resolved field to False
+                    self.resolved = False
+                    # set the status of the associated transaction to still be pending
+                    self.transaction.status = 'With Breakages: Pending Resolution'
+                    self.transaction.save()
+                    # Then save the instance again to reflect the changes
+                    super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
