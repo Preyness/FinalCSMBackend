6fafc18112a636ee6a69c44667f01e47f65c7170
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:07:10 +0800

    added a new path in API

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('equipments/', include('equipments.urls')),
-    path('transactions/', include('transactions.urls'))
+    path('transactions/', include('transactions.urls')),
+    path('breakages/', include('breakages.urls')),
 ]
