f77cfb648754cdc29d0e90e6f4f6fbe04b6104f5
Prince Kurt Laurence <delapenaprincekurtlaurence@gmail.com>
2023-12-11 18:58:33 +0800

    changes in transactions views

--- Backend/transactions/views.py
+++ Backend/transactions/views.py
@@ -1,12 +1,13 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import TransactionSerializer
 from .models import Transaction
-from config.settings import DEBUG
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    if (not DEBUG):
-        permission_classes = [IsAuthenticated]
+    # Only allow GET, POST/CREATE
+    # Transactions cannot be deleted
+    http_method_names = ['get', 'post']
+    permission_classes = [IsAuthenticated]
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
